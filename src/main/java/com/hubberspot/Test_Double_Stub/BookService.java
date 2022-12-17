26d20aaa3d50eee7ddf910c48da03aea6902fd0c
aniruddha_chunne <aniruddha_chunne@calypso.com>
2022-12-17 15:18:48 +0530

    Mockito Potential Exception Stubbing
    Change-Id: I5b9cb26b3013523af4c8abcf795cea45802d3809

---
+++ src/main/java/com/hubberspot/Test_Double_Stub/BookService.java
@@ -0,0 +1,68 @@
+package com.hubberspot.Test_Double_Stub;
+
+import com.hubberspot.dummyTestDoubles.EmailSerivce;
+import com.hubberspot.Test_Double_Stub.Book;
+import java.util.*;
+
+public class BookService
+{
+    private BookRepository bookRepository;
+    private EmailSerivce emailSerivce;
+
+    public BookService(BookRepository bookRepository)
+    {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getNewBooksWithAppliedDiscount (int discountRate, int days)
+    {
+        List <Book> newbook = bookRepository.findNewBooks(days);
+
+        for (Book book: newbook)
+        {
+            int price = book.getPrice();
+            int newPrice = price - (discountRate * price /100);
+            book.setPrice(newPrice);
+        }
+
+        return  newbook;
+    }
+
+//    public void addBook(Book book)
+//    {
+//        bookRepository.save(book);
+//    }
+//
+//    public int findNumberOfBooks()
+//    {
+//        return bookRepository.findAll().size();
+//    }
+
+    public int calculateTotalCost(List <String> bookIds)
+    {
+        int total = 0;
+        for(String bookId: bookIds)
+        {
+
+            Book book = bookRepository.findByBookIds(bookId);
+            total = total + book.getPrice();
+        }
+        return total;
+    }
+
+
+    public void addBook(Book book)
+    {
+        bookRepository.save(book);
+    }
+
+    public void addBook(BookRequest bookRequest)
+    {
+        Book book1 = new Book();
+        book1.setTitle(bookRequest.getTitle());
+        book1.setPrice(bookRequest.getPrice());
+        book1.setPublishedDate(bookRequest.getPublishedDate());
+        bookRepository.save(book1);
+    }
+
+}
---
+++ src/test/java/com/hubberspot/Test_Double_Stub/BookServiceTest.java
@@ -0,0 +1,81 @@
+package com.hubberspot.Test_Double_Stub;
+
+import com.hubberspot.Test_Double_Stub.Book;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest
+{
+        @InjectMocks
+        private BookService bookService;
+
+        @Mock
+        private BookRepository bookRepository;
+
+        @Test
+        public void testCalculatedTotalCostOFBooks()
+        {
+            List<String> bookIds = new ArrayList<>();
+            bookIds.add("1234");
+            bookIds.add("12345");
+
+            Book book1 = new Book("1234", "Mockito in Action", 500, LocalDate.now());
+            Book book2 = new Book("12345", "Junit5 in Action", 400, LocalDate.now());
+
+//            when(bookRepository.findByBookIds("1234")).thenReturn(book1);
+//            when(bookRepository.findByBookIds("12345")).thenReturn(book2);
+            doReturn(book1).when(bookRepository).findByBookIds("1234");
+            doReturn(book2).when(bookRepository).findByBookIds("12345");
+
+
+            int actualcost  = bookService.calculateTotalCost(bookIds);
+
+            assertEquals(900, actualcost);
+
+        }
+
+        @Test
+        public void testSaveBook()
+        {
+            Book book1 = new Book("1234", "Mockito in Action", 500, LocalDate.now());
+            Book book2 = new Book("12345", "Junit5 in Action", 400, LocalDate.now());
+
+            doNothing().when(bookRepository).save(book1);
+            bookService.addBook(book1);
+        }
+
+    @Test
+    public void testSaveBookWithBookRequest()
+    {
+//        Book book1 = new Book("1234", "Mockito in Action", 500, LocalDate.now());
+//        Book book2 = new Book("12345", "Junit5 in Action", 400, LocalDate.now());
+
+//        doNothing().when(bookRepository).save(book1);
+//        bookService.addBook(book1);
+
+        BookRequest bookRequest = new BookRequest("Mockito in Action", 500, LocalDate.now());
+        Book book1 = new Book(null, "Mockito in Action", 500, LocalDate.now());
+
+        doNothing().when(bookRepository).save(book1);
+        bookService.addBook(bookRequest);
+
+
+    }
+
+
+
+
+
+
+}
